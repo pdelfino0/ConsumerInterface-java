d5b42208c20db3c2144b3be8acb8da5645295bd2
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-01-23 19:25:45 -0300

    second commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -20,8 +19,6 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 
 		double factor = 1.1;
-
-		
 
 		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -10,10 +10,6 @@ public class Product {
 		this.price = price;
 	}
 
-	@Override
-	public String toString() {
-		return name + ", " + String.format("%.2f", price);
-	}
 
 	public String getNamme() {
 		return name;
@@ -31,8 +27,9 @@ public class Product {
 		this.price = price;
 	}
 
-	public static void staticPriceUpdate(Product p) {
-		p.setPrice(p.getPrice() * 1.1);
+	@Override
+	public String toString() {
+		return name + ", " + String.format("%.2f", price);
 	}
 
 }
